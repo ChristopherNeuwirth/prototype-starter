60375b36d45d69826bc1627077ec38f515e55997
Christopher Neuwirth <christopher.neuwirth@mhp.com>
2019-09-30 14:13:48 +0200

    test 2

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,28 +1,21 @@
-// import { NestFactory } from '@nestjs/core';
-// import { ExpressAdapter } from '@nestjs/platform-express';
-// import { AppModule } from './app.module';
-// import serverless from 'serverless-http';
-// import express from 'express';
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { AppModule } from './app.module';
+import serverless from 'serverless-http';
+import express from 'express';
 
-// const server = express();
+const server = express();
 
-// server.use('/.netlify/dist'); // path must route to lambda
+server.use('/.netlify/dist'); // path must route to lambda
 
-// export const createNestServer = async expressInstance => {
-//   const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
-//   app.enableCors();
-//   return app.init();
-// };
-
-// createNestServer(server)
-//   .then(v => console.log('Nest Ready'))
-//   .catch(err => console.error('Nest broken', err));
-
-// export const handler = serverless(server);
-
-export const handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: 'Hello, World'
-  });
+export const createNestServer = async expressInstance => {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
+  app.enableCors();
+  return app.init();
 };
+
+createNestServer(server)
+  .then(v => console.log('Nest Ready'))
+  .catch(err => console.error('Nest broken', err));
+
+export const handler = serverless(server);
