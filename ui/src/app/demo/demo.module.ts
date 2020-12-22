9ef37512639de8501d1ed647820fcbb60bf90cb4
Christopher Neuwirth <christopher.neuwirth@mhp.com>
2020-12-22 13:51:57 +0100

    updated angular material to 9

--- ui/src/app/demo/demo.module.ts
+++ ui/src/app/demo/demo.module.ts
@@ -9,13 +9,11 @@ import { environment } from 'src/environments/environment';
 import { ImageLoaderModule } from '../shared/image-loader/image-loader.module';
 import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
 
-import {
-  MatInputModule,
-  MatListModule,
-  MatProgressSpinnerModule,
-  MatButtonModule,
-  MatIconModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { DemoService } from './services/iTunes.service';
 import { ContentfulService } from './services/contentful.service';
